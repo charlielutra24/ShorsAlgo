f0a5246c8189fa459da2e6d94ffa5ffdd6eeb07e
charlielutra24 <charlie.franklin@abingdon.org.uk>
2020-06-11 10:35:16 +0100

    Program now works!

--- main.py
+++ main.py
@@ -1,30 +1,38 @@
 import random
 from euclid_algo import find_gcf
-g = 2  # To save memory, since higher values of g make for much larger numbers in memory, I will use g=2,3,4 etc.
-N = 35
+# To save memory, since higher values of g make for much larger numbers, I will use g=2,3,4 etc.
+g = 2
+N = 217
 done = False
 p = 0
 p_values = []
 while not done:
     p += 1
+    r = g**p % N
     if g**p % N == 1:
+        print(g, p)
         p_values.append(p)
-    if len(p_values) >= 2:
-        print(p_values)
-        if p_values[0] % 1 != 0:
+    if len(p_values) >= 1:
+        p = p_values[0]
+        if p_values[0] % 2 != 0:
             g += 1
             p = 0
             p_values = []
         else:
-            print("HIII!")
-            a = find_gcf(N, g**(p/2)+1)
-            b = find_gcf(N, g**(p/2)-1)
+            x = g**(p/2)+1
+            y = g**(p/2)-1
+            a = int(find_gcf(N, x))
+            b = int(find_gcf(N, y))
             if a == 1 or b == 1:
-                print("HELLO")
                 g += 1
                 p = 0
                 p_values = []
             else:
-                print("hello!")
                 print(g, p, a, b)
                 done = True
+    if p > 500:
+        g += 1
+        p = 0
+        p_values = []
+
+print(a, "x", b, "=", N)
